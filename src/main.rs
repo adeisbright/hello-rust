800d9f95e276a2b45cb2a935dd6ba85e14b5218a
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-01-28 23:36:22 +0100

    Use a Crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use ferris_says::say ; 
+use std::io::{stdout , BufWriter} ; 
 fn main() {
-    println!("Hello, world!");
+    let stdout  = stdout() ; 
+    let message = String::from("Hello Fellow Rustaceans") ;
+    let width = message.chars().count() ; 
+    let mut writer =BufWriter::new(stdout.lock());
+    say(&message , width , &mut writer).unwrap();
 }
