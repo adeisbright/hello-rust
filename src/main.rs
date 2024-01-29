2ea50c54eb3b97b16326cf2d161a33709412a475
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-01-29 22:31:35 +0100

    Build Simple Guess Game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use ferris_says::say ; 
-use std::io::{stdout , BufWriter} ; 
+use std::io::{stdout ,stdin ,  BufWriter} ; 
 fn main() {
     let stdout  = stdout() ; 
     let message = String::from("Hello Fellow Rustaceans") ;
     let width = message.chars().count() ; 
     let mut writer =BufWriter::new(stdout.lock());
     say(&message , width , &mut writer).unwrap();
+
+    println!("Guess the Number Game Comes Below. Now, Guess a number : ");
+    let mut guess = String::new() ;
+    stdin().read_line(&mut guess).expect("Failed to Read Lines");
+    println!("You guessed {guess}");
 }
