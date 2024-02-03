a3755fe8d5cac7d70ffcc5c1f7a4d47c927000a0
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-02-03 16:12:02 +0100

    Work With Data Types and Functions in Rust

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::io::{stdout ,stdin ,  BufWriter} ;
 use rand::Rng; 
 use std::cmp::Ordering ;
 
+fn echo_name(){
+    println!("I am printing out your name") ; 
+}
+
 fn main() {
     let stdout  = stdout() ; 
     let message = String::from("Hello Fellow Rustaceans") ;
@@ -30,4 +34,33 @@ fn main() {
             }
         }
     }
+    //Working with Variables 
+    let y = 10 ;//Y cannot be reassinged a new value 
+    println!("The value of y is {y}");
+    let y = 15 ; //and I also shadowed y 
+    println!("I have now shadowed y : {y}");
+    let mut z = 20; 
+    //I make z to be mutable . Only make a variable mutable if you will mutate it
+    println!("z is now {z}");
+    z = 15 ; 
+    println!("z is {z}");
+    //Array 
+    let scores = [1 , 3 , 4 , 2 , 6] ;
+    for score in scores {
+        println!("The score now is {score}");
+    }
+    echo_name() ;
+    let mut todays_date = String::new() ;
+    stdin().read_line(&mut todays_date).expect("Failed to read todays date");
+    let todays_date : u32 = match todays_date.trim().parse(){
+        Ok(num) => num ,
+        Err(_) => 0,
+    }; 
+
+    if todays_date > 1 {
+        println!("You ought to be in the office");
+    }else{
+        println!("Todays date is {todays_date} : You should be enjoying your weekend");
+    }
+    
 }
