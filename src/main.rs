d18a18552a81ce55a677d58a3f3617ef0da5fe6b
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-02-12 00:40:29 +0100

    Match Pattern: Using the Catch all Pattern

--- src/main.rs
+++ src/main.rs
@@ -201,6 +201,11 @@ fn main() {
         _ => reroll(),
     }
 
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
 
 
 }
